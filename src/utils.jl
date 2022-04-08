999e1a29ed667d06337ad258fa592f12ab7605da
azraq27 <bill.gross@me.com>
2022-04-07 19:02:29 -0500

    added `subbrick`

--- src/utils.jl
+++ src/utils.jl
@@ -9,7 +9,7 @@ prefix(fname::AbstractString) = replace(replace(fname,_nifti_suffix_regex=>""),_
 
 function suffix(fname::AbstractString,suf::AbstractString)
     fname = split(fname,"/")[end]
-    
+
     m = match(_nifti_suffix_regex,fname)
     m != nothing && return fname[1:m.offset-1] * suf * m.match
 
@@ -17,4 +17,23 @@ function suffix(fname::AbstractString,suf::AbstractString)
     m != nothing && return fname[1:m.offset-1] * suf * m.match
 
     return fname * suf
+end
+
+"""subbrick(dset::String,label::String;suff=:coef)
+
+    makes an AFNI-compatible dset string (e.g., "dset[3]") from the dset and label
+    names. If `suff` == `:coef` (the default), will add "_#0Coef" to the label. Also
+    accepts `:tstat` and `:fstat`""" 
+function subbrick(dset::AbstractString,label::AbstractString;suff::Union{T,Symbol,Nothing}=:coef,num::Int=0) where T<:AbstractString
+    info = dset_info(dset)
+    if label != "" && label != nothing
+        if lowercase(String(suff)) != "fstat"
+            label = "$label#$(num)_$(titlecase(String(suff)))"
+        else
+            label = "$(label)_Fstat"
+        end
+    end
+    i = findfirst(isequal(label),info["BRICK_LABS"])
+    i==nothing && return nothing
+    return "$dset[$i]"
 end
